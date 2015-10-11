4588287bb9a951a41eece69ed8d18505c231be53
zhlinh <zhlinhng@gmail.com>
2015-10-11 15:40:15 +0000

    add web data analytics

---
+++ js/addon/analytics.js
@@ -0,0 +1,14 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-68673480-1', 'auto');
+ga('send', 'pageview');
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?23a63ce39976106887d8c0f55a2c7040";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();

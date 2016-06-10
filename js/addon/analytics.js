0b91a927c393b01e20af2389c954ae3b3152df11
zhlinh <zhlinhng@gmail.com>
2016-06-10 06:05:06 +0000

    change the 'highlighter' value to 'rouge'

--- js/addon/analytics.js
+++ js/addon/analytics.js
@@ -4,11 +4,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create', 'UA-68673480-1', 'auto');
 ga('send', 'pageview');
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?23a63ce39976106887d8c0f55a2c7040";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
